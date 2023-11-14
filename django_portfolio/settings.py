879f02752f79f1cb7f3e379e5574b57083a066c0
JuanPablo <jchoternasty@the-8agency.com>
2023-11-14 12:20:41 -0300

    cambios para deploy

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -161,7 +161,7 @@ if not DEBUG:
 # Define la URL pública que se utilizará para acceder a los archivos multimedia desde la aplicación web.
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
